32bb0344d7badd651beeabf42fbfbc70a1778e1f
steven <sarmiento.steven@gmail.com>
2023-10-18 12:46:14 -0400

    ios17 icon animatte

--- Toshi/Core/Components/CircleButton/FavoriteIconView.swift
+++ Toshi/Core/Components/CircleButton/FavoriteIconView.swift
@@ -9,58 +9,59 @@ import SwiftUI
 
     struct FavoriteIconView: View {
         let isFavorited: Bool
-        @State private var isUnfavoriting: Bool = false
-        @State private var scaleEffect: CGFloat = 1.0
+        //@State private var isUnfavoriting: Bool = false
+        //@State private var scaleEffect: CGFloat = 1.0
 
     var body: some View {
-        Image(systemName: getIconName())
+        Image(systemName: isFavorited ? "bookmark.fill" : "bookmark.slash.fill")
             .frame(height: 30)
-            .foregroundColor(getIconColor())
+            .foregroundColor(isFavorited ? .purple : .gray)
             .bold()
             .font(.system(size: 20))
-            .scaleEffect(scaleEffect)
-            .onChange(of: isFavorited, perform: { newValue in
-                if newValue {
-                    withAnimation(.easeIn(duration: 0.1)) {
-                        scaleEffect = 1.2
-                        let impactMed = UIImpactFeedbackGenerator(style: .medium)
-                        impactMed.impactOccurred()
-                    }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        withAnimation(.easeInOut(duration: 0.1)) {
-                            scaleEffect = 1
-                        }
-                    }
-                } else {
-                    isUnfavoriting = true
-                    withAnimation(.easeOut(duration: 0.1)) {
-                        scaleEffect = 1.2
-                        let impactMed = UIImpactFeedbackGenerator(style: .medium)
-                        impactMed.impactOccurred()
-                    }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                        isUnfavoriting = false
-                        withAnimation(.easeInOut(duration: 0.1)) {
-                            scaleEffect = 1
-                        }
-                    }
-                }
-            })
+            //.scaleEffect(scaleEffect)
+            .symbolEffect(.bounce, value: isFavorited)
+//            .onChange(of: isFavorited, perform: { newValue in
+//                if newValue {
+//                    withAnimation(.easeIn(duration: 0.1)) {
+//                        //scaleEffect = 1.2
+//                        let impactMed = UIImpactFeedbackGenerator(style: .medium)
+//                        impactMed.impactOccurred()
+//                    }
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                        withAnimation(.easeInOut(duration: 0.1)) {
+//                            //scaleEffect = 1
+//                        }
+//                    }
+//                } else {
+//                    isUnfavoriting = true
+//                    withAnimation(.easeOut(duration: 0.1)) {
+//                        //scaleEffect = 1.2
+//                        let impactMed = UIImpactFeedbackGenerator(style: .medium)
+//                        impactMed.impactOccurred()
+//                    }
+//                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
+//                        isUnfavoriting = false
+//                        withAnimation(.easeInOut(duration: 0.1)) {
+//                             scaleEffect = 1
+//                        }
+//                    }
+//                }
+//            })
     }
         
-        private func getIconName() -> String {
-            if isUnfavoriting {
-                return "bookmark.slash.fill"
-            } else {
-                return isFavorited ? "bookmark.fill" : "bookmark.fill"
-            }
-        }
-        
-        private func getIconColor() -> Color {
-            if isUnfavoriting {
-                return .gray
-            } else {
-                return isFavorited ? .purple : .gray
-            }
-        }
+//        private func getIconName() -> String {
+//            if isUnfavoriting {
+//                return "bookmark.slash.fill"
+//            } else {
+//                return isFavorited ? "bookmark.fill" : "bookmark.fill"
+//            }
+//        }
+//        
+//        private func getIconColor() -> Color {
+//            if isUnfavoriting {
+//                return .gray
+//            } else {
+//                return isFavorited ? .purple : .gray
+//            }
+//        }
     }
